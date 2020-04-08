aebc1610f2b1d285b042b38b0b4a6fa74edd0c22
NIRBHAY NARAYAN SINGH <44487887+nns15899@users.noreply.github.com>
2020-04-08 23:30:30 +0530

    Delete urls.py

--- Student-Counselling-System-master/home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from home import views
-
-urlpatterns = [
-    path('',views.index,name='index'),
-    path('about',views.about,name='about'),
-]
- 
